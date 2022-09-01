f0bb29ee98066b9eb732d456e01630d07aeec7fe
swapnilbonde94 <94321457+swapnilbonde94@users.noreply.github.com>
2022-09-01 11:23:01 +0530

    Create prog1.sql

---
+++ PLSQL/prog1.sql
@@ -0,0 +1,17 @@
+-- DECLARE
+--     x NUMBER := 40;
+
+-- BEGIN
+--     dbms_output.put_line('The value os x is: ' || x);
+
+-- END;
+-- /
+
+DECLARE
+    var1 varchar(20) := 'I love GeeksforGeeks';
+
+BEGIN
+    dbms_output.put_line(var1);
+
+END;
+/
